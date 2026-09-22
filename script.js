a262def1e895c0d1a9ad348d51541dd93cae53bf
eue892422-tech <eue892422@gmail.com>
2026-09-22 18:10:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ const videoCategoriesData = [
       { src: "videos/trend/video1.mp4", title: "Himmel" },
       { src: "videos/trend/video2.mp4", title: "Frieren" },
       { src: "videos/trend/video3.mp4", title: "Frieren" },
-      { src: "videos/trend/video4.mp4", title: "Eisen" }
+      { src: "videos/trend/video4.mp4", title: "Himmel" }
     ]
   }
 ];
